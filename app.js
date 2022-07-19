f60cd0a996da7e854e77e69eb5136d42c194142b
Kazi Sayeda Umme Aiman Aijazoddin <109507519+aimankazii@users.noreply.github.com>
2022-07-19 15:38:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+gsap.from(".img", {
+    y: -200,
+    duration: 2
+})
+gsap.from(".profile", {
+    x: 400,
+    duration: 2
+})
+gsap.from(".info", {
+    y: 50,
+    duration: 1
+})
+gsap.from(".contact", {
+    x: -20,
+    duration: 2
+})
+gsap.from(".msg1", {
+    y: -20,
+    duration: 2
+})
